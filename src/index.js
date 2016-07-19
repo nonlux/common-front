2f08b035948872d4989017482a259b1a1787db6f
nonlux <nonluxi@gmail.com>
2016-07-19 16:18:18 +0800

    Full stack example

--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
-console.log('Hi. I am very hot update. And I reload now with browser sync. Bey');
-
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
-function Hello() {
-  return <h2>I am react app. And I am working fast. Magic. And booo</h2>
+import EventEmitter from 'events';
+const dispatcher = new EventEmitter;
+
+class Hello extends Component {
+  static propTypes = {
+    foo: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  }
+
+  constructor(props) {
+    super(props);
+    const foo = props.foo ? parseInt(props.foo, 10) : 1;
+    this.state = { foo };
+
+    dispatcher.on('add', (value) => {
+      this.setState({
+        ...this.state,
+        foo: value + this.state.foo
+      });
+    });
+  }
+
+  render() {
+    const { foo } = this.state;
+    return <h2>I am react app with dynamic value: {foo}</h2>;
+  }
 }
 
-ReactDOM.render(<Hello />, document.querySelector('#app'));
+function Button() {
+  const handleClick = () => {
+    dispatcher.emit('add', 1);
+  };
+  return <button onClick={handleClick} >Button</button>;
+}
 
+
+const helloTag = Object.create(HTMLElement.prototype, {
+  createdCallback: {
+    value() {
+      const foo = this.dataset.foo ? this.dataset.foo : 1;
+      ReactDOM.render(<Hello foo={foo} />, this);
+    }
+  }
+});
+
+const ButtonTag = Object.create(HTMLElement.prototype, {
+  createdCallback: {
+    value() {
+      ReactDOM.render(<Button />, this);
+    }
+  }
+});
+
+document.registerElement('x-hello', { prototype: helloTag });
+document.registerElement('x-button', { prototype: ButtonTag });
